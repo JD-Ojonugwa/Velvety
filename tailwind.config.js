120a429444d96d6e4ff6164f1a28e25b465d141f
Joy Abel <joydorothy222@gmail.com>
2024-09-27 11:39:51 +0100

    fix: resolve package.json issues.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this is correct
+  ],
   theme: {
     extend: {},
   },
