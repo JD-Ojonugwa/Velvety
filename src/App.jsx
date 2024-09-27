9a4edc2a4b867f5d68ee3ec2310876b48b232eee
Joy Abel <joydorothy222@gmail.com>
2024-09-27 14:00:23 +0100

    feat: About us

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Component/Header";
 import Hero from "./Component/Hero";
+import About from "./Component/About";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <About />
     </div>
   );
 };
